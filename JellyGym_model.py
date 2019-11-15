ed35761183d193d9bc6b937326e37e4d40ecc8eb
NicholasNapolitano <dark.rai95@hotmail.it>
2019-11-15 15:52:37 +0100

    Update JellyGym_model.py

--- JellyGym_model.py
+++ JellyGym_model.py
@@ -1,24 +1,22 @@
-import numpy as np
-import pandas as pd
+#Import Librerie
+
 import csv
 import pickle
-from numpy import loadtxt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Activation
-from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPool2D
-from tensorflow.keras.layers import AvgPool2D
-from tensorflow.keras.layers import Flatten
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.utils import to_categorical
-from collections import deque
+
+#Creazione Replay Memory
 
 replay_memory = []
+
+#Inizializzazione learning rate
+
 learning_rate = 0.001
-epsilon = 1.0
+
+#Creazione Modello
 
 model = Sequential()
 model.add(Dense(100, use_bias = False, input_dim = 82))
@@ -30,24 +28,20 @@ model.add(Activation('relu'))
 model.add(Dense(324, use_bias = False))
 model.add(BatchNormalization())
 model.add(Activation('softmax')) 
-model.compile(loss='mse', optimizer=Adam(lr=learning_rate), metrics=['mean_squared_error'])
+model.compile(loss='mse', optimizer=Adam(lr=learning_rate), 
+              metrics=['mean_squared_error'])
 model.build()
 
+#Stampa Informazioni sul modello
+
 model.summary()
+
+#Salvataggio del Modello
         
-model.save("//home//nvidia//workspace//nicholasnapolitano//data//MainModel.h5")
+model.save("MainModel.h5")
 
-with open("//home//nvidia//workspace//nicholasnapolitano//data//Replay_Memory.csv", "wb") as fp:   #Pickling
+#Salvataggio Replay Memory
+
+with open("Replay_Memory.csv", "wb") as fp:   #Pickling
     pickle.dump(replay_memory, fp)
-
-
-
-
-
-
-
-
-
-
-
-
+    
