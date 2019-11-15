fcab0f8869d89b806c4abd5214819017e62f8201
NicholasNapolitano <dark.rai95@hotmail.it>
2019-11-15 15:56:17 +0100

    Update JellyGym_duelingModel.py

--- JellyGym_duelingModel.py
+++ JellyGym_duelingModel.py
@@ -1,24 +1,22 @@
-import numpy as np
-import pandas as pd
+#Import Librerie
+
 import csv
 import pickle
-from numpy import loadtxt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Activation
-from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPool2D
-from tensorflow.keras.layers import AvgPool2D
-from tensorflow.keras.layers import Flatten
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import load_model
-from collections import deque
+
+#Creazione Replay Memory
+
+replay_memory = []
+
+#Inizializzazione learning rate
 
 learning_rate = 0.001
-epsilon = 1.0
+
+#Creazione Modello
 
 model = Sequential()
 model.add(Dense(100, use_bias = False, input_dim = 82))
@@ -30,16 +28,19 @@ model.add(Activation('relu'))
 model.add(Dense(324, use_bias = False))
 model.add(BatchNormalization())
 model.add(Activation('softmax')) 
-model.compile(loss='mse', optimizer=Adam(lr=learning_rate), metrics=['mean_squared_error'])
+model.compile(loss='mse', optimizer=Adam(lr=learning_rate), 
+              metrics=['mean_squared_error'])
 model.build()
+
+#Stampa Informazioni sul modello
 
 model.summary()
 
-mainModel = load_model("//home//nvidia//workspace//nicholasnapolitano//data//MainModel.h5")
+#Aggiornamento dei pesi copiandoli dalla rete principale
+
+mainModel = load_model("MainModel.h5")
 model.set_weights(mainModel.get_weights())
 
-model.save("//home//nvidia//workspace//nicholasnapolitano//data//DuelingModel.h5")
+#Salvataggio del Modello
 
-
-
-
+model.save("DuelingModel.h5")
